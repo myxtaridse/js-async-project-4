04d38c6eecfcfa26ddd2976ae9261154196ce020
Ksenia Durkina <sigismund1old@gmail.com>
2026-07-14 11:26:17 +0300

    feat: add parser for targetUrl and make a request to receive and write to a file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-export default () => {}
+import axios from 'axios'
+import { writeFile } from 'fs/promises'
+import path from 'path'
+
+const parseUrl = (targetUrl) => {
+  const href = targetUrl.replace(/https?:\/\//, '')
+  const newHref = href.replace(/[^A-Za-z0-9]/g, '-')
+  return `${newHref}.html`
+}
+
+export default (targetUrl, outputDir = process.cwd()) => {
+  const filename = parseUrl(targetUrl)
+  const filepath = path.join(outputDir, filename)
+  return axios.get(targetUrl)
+    .then(({ data }) => writeFile(filepath, data))
+    .then(() => filepath)
+}
