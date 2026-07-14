901b3958f0c58463cd296022045874f9eeaed5e6
Ksenia Durkina <sigismund1old@gmail.com>
2026-07-14 11:29:38 +0300

    style: lint-fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,13 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import { defineConfig } from 'eslint/config'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default defineConfig([
   stylistic.configs.recommended,
   {
-    files: ["**/*.{js,mjs,cjs}"],
+    files: ['**/*.{js,mjs,cjs}'],
     plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: { ...globals.node, ...globals.jest } } }
-]);
+    extends: ['js/recommended'],
+    languageOptions: { globals: { ...globals.node, ...globals.jest } } },
+])
